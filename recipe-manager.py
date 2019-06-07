410591ff0c945d5fd148e0d807db71c2830d5691
y4izus <yaiza.garcia.mm@gmail.com>
2019-06-07 16:28:43 +0200

    Retrieves recipes with an specific food

--- recipe-manager.py
+++ recipe-manager.py
@@ -4,15 +4,16 @@ import argparse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 def _has(food):
     _has.description = 'search recipes with {}'.format(food)
-    print(food)
-    url = 'https://www.recetario.es/encontrar?rec_all=0&search={food}'
+    url = f'https://www.recetario.es/encontrar?rec_all=0&search={food}'
     html = urlopen(url)
     soup = BeautifulSoup(html, 'lxml')
-    recipes = soup.select('a[class="item-link item-title"]')
+    recipes_links = soup.select('a[class="item-link item-title"]')
 
-    return recipes
+    return recipes_links
+
 
 def _build_parser():
     parser = argparse.ArgumentParser(
@@ -20,21 +21,22 @@ def _build_parser():
     
     # options
     parser.add_argument(
-        '-has', action='store_true',
+        '-has',
+        action='store_true',
         help='indicates the foods that the recipes must contain')
     parser.add_argument('food')
     
     return parser
+
 
 if __name__ == '__main__':
     parser = _build_parser()
     args = parser.parse_args()
 
     if args.has:
-      result = _has(args.food)
-      recipes = [recipe.text for recipe in result]
+        recipes = _has(args.food)
 
-      for recipe in recipes:
-        print(recipe)
-
-    # print(args)
+        for recipe in recipes:
+            recipe_title = recipe.text.capitalize()
+            recipe_link = recipe.attrs['href']
+            print(f'{recipe_title} en https://www.recetario.es{recipe_link}')
