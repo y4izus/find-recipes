905a3c9c88183d80ebdcc84cc8292699be466d31
y4izus <yaiza.garcia.mm@gmail.com>
2019-07-16 16:31:11 +0200

    refactor

--- recipe_manager.py
+++ recipe_manager.py
@@ -7,6 +7,11 @@ from urllib.parse import ParseResult, urlencode, urlunparse
 from PyInquirer import prompt
 
 
+def create_url(path, query=''):
+    return urlunparse(ParseResult(scheme='https', netloc='www.recetario.es', path=path,
+                                  params='', query=urlencode(query), fragment=''))
+
+
 def get_recipes_for(food):
     """
     Return the set of recipe links containing ``food`` by scrapping a certain web page.
@@ -16,6 +21,7 @@ def get_recipes_for(food):
         'rec_all': '0',
         'search': food
     }
+    # url_parsed = create_url('/encontrar', query)
     url_parsed = urlunparse(ParseResult(scheme='https', netloc='www.recetario.es', path='/encontrar',
                                         params='', query=urlencode(query), fragment=''))
 
@@ -24,6 +30,18 @@ def get_recipes_for(food):
     recipes_links = soup.select('a[class="item-link item-title"]')
 
     return set(recipes_links)
+
+
+def get_recipe_ingredients(recipe_link):
+    url = create_url(recipe_link)
+    print(recipe_link)
+    html = urlopen(url)
+    scraper = BeautifulSoup(html, 'lxml')
+    ingredients_scraped = scraper.select('li[itemprop="recipeIngredient"]')
+    ingredients_list = [ingredient.text.replace('\n', '').replace('\t', '').strip()
+                        for ingredient in ingredients_scraped]
+
+    print(ingredients_list)
 
 
 def _build_parser():
@@ -50,7 +68,8 @@ def show_recipes_list_on_console(recipes_list):
     ]
     answers = prompt(questions)
     url_selected_recipe = answers['selected_recipe']
-    print(url_selected_recipe)
+    get_recipe_ingredients(url_selected_recipe)
+    # print(url_selected_recipe)
 
 
 def format_recipes_to_show_in_console(recipes):
@@ -64,7 +83,7 @@ def format_recipes_to_show_in_console(recipes):
         recipe_link = recipe.attrs['href']
         recipes_formatted_to_console.append(
             {
-                'value': f'https://www.recetario.es{recipe_link}',
+                'value': recipe_link,
                 'name': recipe_title
             })
     return recipes_formatted_to_console
